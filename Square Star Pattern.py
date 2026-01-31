c86c745078e56c87b203306d18317b1e3349295b
MEGHA SREE VENKATTACHALAM <meghavenkattachalam@gmail.com>
2026-01-31 12:07:23 +0530

    Create Square Star Pattern.py

---
+++ Square Star Pattern.py
@@ -0,0 +1,13 @@
+class solution:
+    def printSquarePattern(self, n):
+        #Write your code here...
+        
+        
+        for i in range(n):
+            print("* "*(n))
+
+OUTPUT:
+
+* * * 
+* * * 
+* * * 
