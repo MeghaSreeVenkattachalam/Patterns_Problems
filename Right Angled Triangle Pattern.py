7c7ef367c2ad093a77337f9316bba9d3b417bd50
MEGHA SREE VENKATTACHALAM <meghavenkattachalam@gmail.com>
2026-01-31 12:09:29 +0530

    Create Right Angled Triangle Pattern.py

---
+++ Right Angled Triangle Pattern.py
@@ -0,0 +1,11 @@
+class solution:
+    def printRightAngledTrianglePattern(self, n):
+        #Write your code here...
+        for i in range(n):
+            print("* "*(i+1))
+
+OUTPUT:
+
+* 
+* * 
+* * * 
