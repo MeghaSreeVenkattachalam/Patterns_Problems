feab91a96c5568716b65c05bcb76a5e2610a18e3
MEGHA SREE VENKATTACHALAM <meghavenkattachalam@gmail.com>
2026-01-31 12:14:38 +0530

    Add solution class for right-angled number triangle
    Implement a class to print a right-angled number triangle.

---
+++ Right Angled Number Triangle.py
@@ -0,0 +1,15 @@
+class solution:
+    def printRightAngledNumberTriangle(self, n):
+        #Write your code here...
+        
+        
+        for i in range(1,n+1):
+            for j in range(1,i+1):
+                print(i,end=" ")
+            print()
+OUTPUT:
+
+1 
+2 2 
+3 3 3 
+4 4 4 4 
