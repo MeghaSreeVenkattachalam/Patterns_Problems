db4883570e84e9971c56ab269ec32ec8695649b4
MEGHA SREE VENKATTACHALAM <meghavenkattachalam@gmail.com>
2026-01-31 12:15:52 +0530

    Add inverted right-angled triangle pattern method
    Implement a method to print an inverted right-angled triangle pattern.

---
+++ InvertedRightAngledTrianglePattern.py
@@ -0,0 +1,13 @@
+class solution:
+        def printInvertedRightAngledTrianglePattern(self, n):
+        #Write your code here...
+        for i in range(n,-1,-1):
+            print("* "*i)
+
+OUTPUT:
+
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
