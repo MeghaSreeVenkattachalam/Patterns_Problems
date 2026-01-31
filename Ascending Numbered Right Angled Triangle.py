a3c83450ad541fdfa0a9dead001ac5caf1d557d0
MEGHA SREE VENKATTACHALAM <meghavenkattachalam@gmail.com>
2026-01-31 14:44:24 +0530

    Create Ascending Numbered Right Angled Triangle.py

---
+++ Ascending Numbered Right Angled Triangle.py
@@ -0,0 +1,18 @@
+class solution:
+    def printAscendingNumberTriangle(self, n):
+        #Write your code here...
+        val = 1
+        for i in range(1,n+1):
+            for j in range(1,i+1):
+                print(val,end=" ")
+                val+=1
+            print()
+
+
+OUTPUT:
+
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15 
